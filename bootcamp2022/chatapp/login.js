a68c9d4ab9e1b23c0fdc0a542b6503f663f214d3
itz.keshav <82344541+Keshav11-coder@users.noreply.github.com>
2022-07-02 14:02:59 -0300

    Delete bootcamp2022/chatapp directory

--- bootcamp2022/chatapp/login.js
+++
@@ -1,9 +0,0 @@
-function getInput(){
-var USER = document.getElementById('logon-as').value;
-var ID = document.getElementById('admin').value;
-if(USER == "keshav"){
-    document.getElementById("content").className="content";
-}
-}
-
-setInterval(getInput(), 1000);
